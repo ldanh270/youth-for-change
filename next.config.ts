49ababe0ae0885d98c71e424a2fdeeeb9b2f355b
ldanh270 <ducanhle.dn@gmail.com>
2026-01-06 21:47:26 +0700

    chore(next.config.ts): allow res.cloudinary.com & www.notion.so in next/image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
+
+    // Image configuration
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+            },
+            {
+                protocol: "https",
+                hostname: "www.notion.so",
+            },
+        ],
+        dangerouslyAllowSVG: true,
+        contentDispositionType: "attachment",
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    },
 }
 
 export default nextConfig
