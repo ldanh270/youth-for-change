60ea4755e1b8fbda2c3d38a0436306671a932052
ldanh270 <ducanhle.dn@gmail.com>
2026-01-06 14:33:52 +0700

    chore(notion): remove redundant databaseId in configs

--- src/configs/notion.ts
+++ src/configs/notion.ts
@@ -1,8 +1,5 @@
 // Token to access with user permision (Config in https://www.notion.so/my-integrations)
 export const notionToken = process.env.NOTION_TOKEN || ""
 
-// Workspace database id
-export const databaseId = process.env.NOTION_DATABASE_ID || ""
-
 // Blog data source id
 export const blogDataSourceId = process.env.NOTION_BLOGS_DATA_SOURCES_ID || ""
