41b2dcfdc24af9531bfdea2945aa849156be1de4
ldanh270 <ducanhle.dn@gmail.com>
2026-01-10 09:24:03 +0700

    feat: add flipbook

---
+++ src/app/(feature)/flipbook/page.tsx
@@ -0,0 +1,11 @@
+export default function FlipbookPage() {
+    return (
+        <main className="relative h-[90vh]">
+            <iframe
+                className="absolute top-0 left-0 h-full w-full"
+                src="https://heyzine.com/flip-book/8c5eee422c.html"
+                allowFullScreen
+            />
+        </main>
+    )
+}
